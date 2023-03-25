c7d1349f40bc10037925cc8ca5ff9163b1017374
dnslin <dnslint@gmail.com>
2023-03-25 20:17:11 +0800

    feat: add js demo

---
+++ js/data.js
@@ -0,0 +1,22 @@
+/**
+ * @name: data.js
+ * @projectName: ReactProject
+ * @description:
+ * @dirPath: js
+ * @author: DnsLin
+ * @organizationName: dnslin.com
+ * @time: 2023/3/22
+ */
+// 一个数组，包含了所有书籍的信息
+const bookList = [
+    {id: '1', name: 'JavaScript高级程序设计', data: '2022-02-01', price: 99.99, count: 5},
+    {id: '2', name: 'JavaScript权威指南', data: '2022-03-15', price: 79.99, count: 3},
+    {id: '3', name: 'JavaScript设计模式', data: '2022-01-20', price: 69.99, count: 7},
+    {id: '4', name: 'JavaScript语言精粹', data: '2022-04-05', price: 49.99, count: 8},
+    {id: '5', name: 'Node.js实战', data: '2022-05-15', price: 89.99, count: 2},
+    {id: '6', name: 'Vue.js实战', data: '2022-06-10', price: 59.99, count: 6},
+    {id: '7', name: 'React.js实战', data: '2022-07-20', price: 69.99, count: 4},
+    {id: '8', name: 'TypeScript实战', data: '2022-08-10', price: 99.99, count: 3},
+    {id: '9', name: 'Angular实战', data: '2022-09-05', price: 79.99, count: 5},
+    {id: '10', name: 'Express.js实战', data: '2022-10-15', price: 69.99, count: 7},
+];
